5fd65ccf987627b2d3bd4c7ec538e95c7a2fe96d
Andrey Naldoni <andreynaldoni@gmail.com>
2023-04-27 09:37:05 -0300

    💄 Added projects section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,27 @@
 import {
   AppShell,
+  Box,
   Button,
   Container,
   Group,
   Image,
+  Paper,
   Stack,
+  Text,
   Title,
 } from '@mantine/core';
-import { IconDownload, IconFileSearch } from '@tabler/icons';
+import { useMediaQuery } from '@mantine/hooks';
+import { IconChevronRight, IconDownload, IconFileSearch } from '@tabler/icons';
+import { Link } from 'react-router-dom';
+
 import GitHubButton from 'components/GitHubButton';
 import HeaderNavbar from 'components/HeaderNavbar';
 import LinkedInButton from 'components/LinkedInButton';
 import MailButton from 'components/MailButton';
 import WhatsAppButton from 'components/WhatsAppButton';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const isMobile = useMediaQuery('(max-width: 767px)');
   return (
     <AppShell
       // padding="md"
@@ -77,6 +83,75 @@ const Home = () => {
               View resume online
             </Button>
           </Group>
+          <Title weight={600} color="blue" align="center">
+            Projects
+          </Title>
+          <Paper
+            radius="md"
+            withBorder
+            shadow="sm"
+            sx={{
+              overflow: 'hidden',
+            }}
+          >
+            <Group
+              sx={{
+                display: 'flex',
+                flexDirection: isMobile ? 'column' : 'row',
+              }}
+              spacing={0}
+            >
+              <Box
+                component="a"
+                href="https://nextpic.app"
+                sx={{
+                  width: isMobile ? '100%' : '50%',
+                }}
+              >
+                <Image
+                  src={process.env.PUBLIC_URL + '/img/nextpic.jpg'}
+                  fit="contain"
+                  alt="NextPic.app"
+                />
+              </Box>
+              <Box
+                p="lg"
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: isMobile ? '100%' : '50%',
+                }}
+              >
+                <Title weight={600} mb="sm">
+                  NextPic.app
+                </Title>
+                <Text size="xl" mb="sm">
+                  Free online photo proofing, available in 🇧🇷 and 🇺🇸 languages.
+                </Text>
+                <Text
+                  size="xl"
+                  variant="link"
+                  component="a"
+                  href="https://nextpic.app"
+                  sx={{ display: 'flex' }}
+                  arial-label="NextPic.app website"
+                  role="link"
+                >
+                  See more
+                  <span
+                    style={{
+                      marginLeft: 4,
+                      display: 'flex',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <IconChevronRight size={22} />
+                  </span>
+                </Text>
+              </Box>
+            </Group>
+          </Paper>
         </Stack>
       </Container>
     </AppShell>
